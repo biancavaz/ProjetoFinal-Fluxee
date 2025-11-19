dbbcf84ba6a7c12838216e6ada81fb84c4b7f9b3
biancavaz <147559714+biancavaz@users.noreply.github.com>
2025-11-18 22:42:38 -0300

    ajustado

--- criar_admin.py
+++ criar_admin.py
@@ -1,18 +1,30 @@
+from datetime import date
 from app import app, db, bcrypt
 from app.models import User
 
 def criar_admin():
+    # Verifica se o admin já existe
+    admin_existente = User.query.filter_by(email='admin@site.com').first()
+    if admin_existente:
+        print("Usuário admin já existe!")
+        return
+
     # Criar hash da senha
     senha_hash = bcrypt.generate_password_hash('admin123').decode('utf-8')
 
     # Criar usuário admin
     admin = User(
         nome='Admin',
-        sobrenome='Principal',
-        email='admin@teste.com',
+        data_nascimento=date(2000, 1, 1),  # objeto date
+        genero='Outro',
+        email='admin@site.com',
+        cpf='00000000000',
+        telefone='0000000000',
+        tipo_usuario='admin',
         senha=senha_hash,
-        tipo='admin'
+        imagem_perfil=None
     )
+
 
     # Salvar no banco
     db.session.add(admin)
